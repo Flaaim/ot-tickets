7c61fe07764825acf5cd9f631613181b21fbc21e
Flaaim <flaaim@list.ru>
2025-07-09 08:50:05 +1000

    install Dotenv, set ENV

--- config/dependencies.php
+++ config/dependencies.php
@@ -2,10 +2,20 @@
 
 declare(strict_types=1);
 
-$files = array_merge(
-    glob(__DIR__ . '/common/*.php'),
-    glob(__DIR__ . '/' . (getenv('APP_ENV') ?: 'prod') . '/*.php') ?: []
-);
+
+use Dotenv\Dotenv;
+
+$app_env = getenv('APP_ENV');
+$file = ($app_env === 'dev') ? "dev.env" : ".env";
+if (file_exists(__DIR__ . '/common/env/' . $file)) {
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/common/env/', $file);
+    $dotenv->load();
+} else {
+    exit(".Env file not found");
+}
+
+$files = glob(__DIR__ . '/common/*.php');
+
 
 $configs = array_map(
     static function ($file){
