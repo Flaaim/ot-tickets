c087a47acd3164b8f722c2d64456689ca8432f8b
Flaaim <flaaim@list.ru>
2025-07-09 09:00:02 +1000

    use Autowiring

--- config/container.php
+++ config/container.php
@@ -5,5 +5,9 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 
 $builder = new ContainerBuilder();
+
+$builder->useAutowiring(true);
+
 $builder->addDefinitions(require __DIR__ . '/dependencies.php');
+
 return $builder->build();
