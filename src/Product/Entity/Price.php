8f2c359cba602e720c57bd47b9e8d5f91fec91fa
Flaaim <flaaim@list.ru>
2025-07-27 19:05:42 +1000

    add Price.php and PriceTest.php.php

--- src/Product/Entity/Price.php
+++ src/Product/Entity/Price.php
@@ -8,26 +8,26 @@ use Webmozart\Assert\Assert;
 final class Price
 {
     private float $value;
-    private string $currency;
-    public function __construct(float $value, string $currency = 'RUB')
+    public function __construct(float $value)
     {
         Assert::greaterThan($value, 0, 'Price must be positive');
-        Assert::length($currency, 3, 'Currency must be 3-letter code (e.g. RUB)');
         $this->value = round($value, 2); // Округляем до копеек/центов
-        $this->currency = mb_strtoupper($currency);
     }
 
     public function getValue(): float
     {
         return $this->value;
     }
-    public function getCurrency(): string
-    {
-        return $this->currency;
-    }
-
     public function add(self $other): self
     {
-        return new self($this->value + $other->value, $this->currency);
+        return new self($this->value + $other->value);
+    }
+    public function subtract(self $other): self
+    {
+        return new self($this->value - $other->value);
+    }
+    public function equals(self $other): bool
+    {
+        return $this->value === $other->value;
     }
 }
---
+++ src/Product/Test/Entity/PriceTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Product\Test\Entity;
+
+use App\Product\Entity\Price;
+use PHPUnit\Framework\TestCase;
+
+class PriceTest extends TestCase
+{
+    public function testPrice(): void
+    {
+        $price = new Price($value = 12.34);
+
+        $this->assertNotNull($price->getValue());
+        $this->assertEquals($value, $price->getValue());
+    }
+
+    public function testZero(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Price(0);
+    }
+
+    public function testAddPrice(): void
+    {
+        $price = (new Price($value = 12.34))->add(new Price($other = 12.34));
+        $sum = $value + $other;
+
+        $this->assertEquals($sum, $price->getValue());
+    }
+    public function testSubtractPrice(): void
+    {
+        $price = (new Price($value = 12.34))->subtract(new Price($other = 12.34));
+        $subtract = $value - $other;
+        $this->assertEquals($subtract, $price->getValue());
+    }
+
+    public function testEqualPrice(): void
+    {
+        $price = new Price(12.34);
+
+        $other = new Price(12.34);
+
+        $this->assertTrue($price->equals($other));
+    }
+}
