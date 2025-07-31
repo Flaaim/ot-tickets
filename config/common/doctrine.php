408a465f9eae48f42f34dc94dc8edefc8bad3124
Flaaim <flaaim@list.ru>
2025-07-31 14:39:11 +1000

    Added manager configuration

---
+++ config/common/doctrine.php
@@ -0,0 +1,48 @@
+<?php
+
+
+declare(strict_types=1);
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
+use Doctrine\ORM\ORMSetup;
+use Psr\Container\ContainerInterface;
+
+
+return [
+
+    EntityManagerInterface::class => function (ContainerInterface $container): EntityManagerInterface {
+        $settings = $container->get('config')['doctrine'];
+
+        $config = ORMSetup::createAttributeMetadataConfiguration(
+            $settings['metadata_dirs'],
+            $settings['dev_mode'],
+            $settings['proxy_dir'],
+            $settings['cache_dir'],
+        );
+
+        $config->setNamingStrategy(new UnderscoreNamingStrategy());
+
+        $connection = DriverManager::getConnection(
+            $settings['connection'],
+            $config
+        );
+
+        return new EntityManager($connection, $config);
+    },
+    'config' => [
+        'doctrine' => [
+            'dev_mode' => false,
+            'cache_dir' => __DIR__ . '/../../var/cache/doctrine/cache',
+            'proxy_dir' => __DIR__ . '/../../var/cache/doctrine/proxy',
+            'connection' => [
+                'driver' => 'pdo_pgsql',
+                'url' => getenv('DB_URL'),
+                'charset' => 'utf-8'
+            ],
+            'metadata_dirs' => [],
+        ],
+    ],
+];
