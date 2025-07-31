565b4fd7fa80d1749f8f750bf6a38f65a17071ca
Flaaim <flaaim@list.ru>
2025-07-31 19:58:26 +1000

    Added DB configuration

--- config/common/doctrine.php
+++ config/common/doctrine.php
@@ -8,11 +8,13 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
 use Doctrine\ORM\ORMSetup;
+use Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
 use Psr\Container\ContainerInterface;
 
 
 return [
-
     EntityManagerInterface::class => function (ContainerInterface $container): EntityManagerInterface {
         $settings = $container->get('config')['doctrine'];
 
@@ -39,10 +41,16 @@ return [
             'proxy_dir' => __DIR__ . '/../../var/cache/doctrine/proxy',
             'connection' => [
                 'driver' => 'pdo_pgsql',
-                'url' => getenv('DB_URL'),
+                'host' => $_ENV['DB_HOST'],
+                'user' => $_ENV['DB_USER'],
+                'password' => $_ENV['DB_PASSWORD'],
+                'dbname' => $_ENV['DB_NAME'],
                 'charset' => 'utf-8'
             ],
             'metadata_dirs' => [],
         ],
     ],
+    EntityManagerProvider::class => function (ContainerInterface $container): SingleManagerProvider {
+        return new SingleManagerProvider($container->get(EntityManagerInterface::class));
+    },
 ];
