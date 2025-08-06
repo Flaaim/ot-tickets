f4db69dd2e5d3f4a2208c29b76497831299b0dfc
Flaaim <flaaim@list.ru>
2025-08-06 16:28:40 +1000

    removed use from app.php

--- bin/app.php
+++ bin/app.php
@@ -2,15 +2,8 @@
 
 declare(strict_types=1);
 
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand;
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Doctrine\ORM\Tools\Console\EntityManagerProvider;
-use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\FormatterHelper;
-use Symfony\Component\Console\Helper\HelperSet;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -19,7 +12,6 @@ $container = require __DIR__ . '/../config/container.php';
 $cli = new Application('Console');
 
 $commands = $container->get('config')['console']['commands'];
-$entityManagerProvider = $container->get(EntityManagerProvider::class);
 
 foreach ($commands as $name) {
     /** @var Command $command */
