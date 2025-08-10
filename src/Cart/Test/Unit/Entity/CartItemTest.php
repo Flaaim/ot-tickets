136c27c2b1967b74f031caa1f02856ff14eb8504
Flaaim <flaaim@list.ru>
2025-08-10 11:29:41 +1000

    added Cart entity tests (Cart.php, CartItemTest.php)

--- src/Cart/Test/Unit/Entity/CartItemTest.php
+++ src/Cart/Test/Unit/Entity/CartItemTest.php
@@ -2,12 +2,11 @@
 
 namespace App\Cart\Test\Unit\Entity;
 
+use App\Cart\Entity\Cart;
 use App\Cart\Entity\CartItem;
-use App\Cart\Entity\Item;
 use App\Product\Test\Builder\ProductBuilder;
 use App\Shared\Domain\ValueObject\Id;
 use PHPUnit\Framework\TestCase;
-use Ramsey\Uuid\Uuid;
 
 class CartItemTest extends TestCase
 {
@@ -22,6 +21,32 @@ class CartItemTest extends TestCase
         $this->assertEquals($id, $item->getId());
         $this->assertSame($product, $item->getProduct());
         $this->assertSame($quantity, $item->getQuantity());
+    }
 
+    public function testIsEquals(): void
+    {
+        $product = (new ProductBuilder())->build();
+
+        $item = new CartItem(ID::generate(), $product);
+        $equalsItem = new CartItem(ID::generate(), clone $product);
+        $notEqualsItem = new CartItem(ID::generate(), (new ProductBuilder())->build());
+
+
+        $this->assertTrue($item->isEqualTo($item));
+        $this->assertTrue($item->isEqualTo($equalsItem));
+
+        $this->assertFalse($item->isEqualTo($notEqualsItem));
+    }
+
+    public function testSetCart(): void
+    {
+        $product = (new ProductBuilder())->build();
+        $item = new CartItem(ID::generate(), $product);
+
+        $cart = new Cart(ID::generate());
+        $item->setCart($cart);
+
+        $this->assertNotNull($item->getCart());
+        $this->assertSame($cart, $item->getCart());
     }
 }
--- src/Cart/Test/Unit/Entity/CartTest.php
+++ src/Cart/Test/Unit/Entity/CartTest.php
@@ -5,7 +5,11 @@ namespace App\Cart\Test\Unit\Entity;
 use App\Auth\Entity\User;
 use App\Auth\Test\Builder\UserBuilder;
 use App\Cart\Entity\Cart;
+use App\Cart\Entity\CartItem;
+use App\Product\Entity\Product;
+use App\Product\Test\Builder\ProductBuilder;
 use App\Shared\Domain\ValueObject\Id;
+use DomainException;
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid;
 
@@ -17,12 +21,38 @@ class CartTest extends TestCase
             $id = new Id(Uuid::uuid4()->toString()),
         );
 
+        $this->assertNotNull($cart->getId());
         $this->assertEquals($cart->getId(), $id);
 
     }
-
-    private function getUser(): User
+    public function testAddToCart(): void
     {
-        return (new UserBuilder())->build();
+        $cart = new Cart(ID::generate());
+
+        $item = new CartItem(ID::generate(), (new ProductBuilder())->build());
+        $cart->addItem($item);
+
+        $this->assertCount(1, $cart->getItems());
+        $this->assertEquals($item, $cart->getItems()[0]);
+    }
+    public function testAddExistingItem(): void
+    {
+        $cart = new Cart(ID::generate());
+        $product = (new ProductBuilder())->build();
+
+        $item = new CartItem(ID::generate(), $product);
+
+        $cart->addItem($item);
+
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage("Item already exists.");
+        $cart->addItem($item);
+    }
+
+    public function testEmptyCart(): void
+    {
+        $cart = new Cart(ID::generate());
+
+        $this->assertEmpty($cart->getItems());
     }
 }
