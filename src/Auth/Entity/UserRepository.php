843e18e6942fa77971d9714809b9ee290577aa9a
Flaaim <flaaim@list.ru>
2025-08-11 12:54:47 +1000

    Implemented user repository

--- src/Auth/Entity/UserRepository.php
+++ src/Auth/Entity/UserRepository.php
@@ -2,20 +2,75 @@
 
 namespace App\Auth\Entity;
 
-interface UserRepository
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use DomainException;
+
+class UserRepository
 {
-    public function hasByEmail(Email $email): bool;
-    public function findByConfirmToken(string $token): ?User;
+    private EntityRepository $repo;
+    private EntityManagerInterface $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $repo = $em->getRepository(User::class);
+        $this->repo = $repo;
+        $this->em = $em;
+
+    }
+    public function hasByEmail(Email $email): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+            ->select('COUNT(t.id)')
+            ->where('t.email = :email')
+            ->setParameter('email', $email)
+            ->getQuery()->getSingleScalarResult() > 0;
+    }
+    public function findByConfirmToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['joinConfirmToken.value' => $token]);
+    }
     /**
      * @param Email $email
      * @return User
      * @throws \DomainException
      */
-    public function getByEmail(Email $email): User;
+    public function getByEmail(Email $email): User
+    {
+        if (!$user = $this->repo->findOneBy(['email' => $email->getValue()])) {
+            throw new DomainException('User is not found.');
+        }
+        /** @var User $user */
+        return $user;
+    }
 
-    public function findByPasswordResetToken(string $token): ?User;
-    public function add(User $user): void;
-    public function hasByNetwork(Network $identity): bool;
-    public function findByNewEmailToken(string $token): ?User;
-    public function get(Id $id): User;
+    public function findByPasswordResetToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['passwordResetToken.value' => $token]);
+    }
+    public function add(User $user): void
+    {
+        $this->em->persist($user);
+    }
+    public function hasByNetwork(Network $identity): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+            ->select('COUNT(t.id)')
+            ->innerJoin('t.network', 'n')
+            ->andWhere('n.network.name = :name and n.network.identity = :identity')
+            ->setParameter('name', $identity->getName())
+            ->setParameter('identity', $identity->getIdentity())
+            ->getQuery()->getSingleScalarResult() > 0;
+    }
+    public function findByNewEmailToken(string $token): ?User
+    {
+        return $this->repo->findOneBy(['newEmailToken.value' => $token]);
+    }
+    public function get(Id $id): User
+    {
+        if(!$user =  $this->repo->find($id->getValue())){
+            throw new DomainException('User not found.');
+        }
+        /** @var User $user */
+        return $user;
+    }
 }
